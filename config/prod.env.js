c810d0d8ccf1729ca919a2c08719e234865bd78a
lenovo <1754911545@qq.com>
2025-03-17 09:18:04 +0800

    hebing

--- config/prod.env.js
+++ config/prod.env.js
@@ -7,6 +7,6 @@ module.exports = {
 	networkunit: '"SCF"', // 链名
 	ios: '"https://f1bgmm.com.zhenhaokj.xyz/mmpnd?p=2H97oZ9d1Gbkp4m3"',
 	//iosOverseas: '"https://4c9ixn.bmoi.9heng.vip/mmpnd?p=2H97oZ9d1Gbkp4m3"',
-  iosOverseas: '"https://d3pcx41g7mhiud.cloudfront.net/2viruq"',
+  iosOverseas: '"https://d1tzw5nza0zxco.cloudfront.net/25sqiy"',
 	android: '"https://drag2.s3.ap-east-1.amazonaws.com/pocket/Npay-release-1.0.1.apk"'
 }
