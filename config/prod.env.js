f643c2573cf5d5a409ed4721c7d899761f761bc6
Tao <1428721461@qq.com>
2025-04-15 21:51:11 +0800

    更新

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,12 +1,13 @@
 // 正式服配置
 // 'use strict'
 module.exports = {
-	NODE_ENV: '"production"',
-	baseURL: '"https://cs.novai.finance"',
-	network: 28800, // 链id
-	networkunit: '"SCF"', // 链名
-	ios: '"https://f1bgmm.com.zhenhaokj.xyz/mmpnd?p=2H97oZ9d1Gbkp4m3"',
-	//iosOverseas: '"https://4c9ixn.bmoi.9heng.vip/mmpnd?p=2H97oZ9d1Gbkp4m3"',
+  NODE_ENV: '"production"',
+  baseURL: '"https://cs.novai.finance"',
+  network: 28800, // 链id
+  networkunit: '"SCF"', // 链名
+  ios: '"https://f1bgmm.com.zhenhaokj.xyz/mmpnd?p=2H97oZ9d1Gbkp4m3"',
+  //iosOverseas: '"https://4c9ixn.bmoi.9heng.vip/mmpnd?p=2H97oZ9d1Gbkp4m3"',
   iosOverseas: '"https://d1tzw5nza0zxco.cloudfront.net/25sqiy"',
-	android: '"https://drag2.s3.ap-east-1.amazonaws.com/pocket/Npay-release-1.0.1.apk"'
-}
+  android:
+    '"https://drag2.s3.ap-east-1.amazonaws.com/pocket/Npay-release-1.0.1.apk"'
+};
