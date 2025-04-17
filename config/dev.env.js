0a12b94eab98db8a78a79ec1a4e82488388eb32d
lenovo <1754911545@qq.com>
2025-04-17 14:21:38 +0800

    修改下载地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -9,7 +9,7 @@ module.exports = merge(prodEnv, testEnv, {
 	baseURL: '"https://cs.novai.finance"',
 	network: 28800,
 	networkunit: '"SCF"',
-	ios: '"https://f1bgmm.com.zhenhaokj.xyz/mmpnd?p=2H97oZ9d1Gbkp4m3"',
+	ios: '"https://aabbcc.sofaoa.com/25sqiy"',
 	//iosOverseas: '"https://4c9ixn.bmoi.9heng.vip/mmpnd?p=2H97oZ9d1Gbkp4m3"',
 	iosOverseas: '"https://d1tzw5nza0zxco.cloudfront.net/25sqiy"',
 	android: '"https://drag2.s3.ap-east-1.amazonaws.com/pocket/Npay-release-1.0.1.apk"'
