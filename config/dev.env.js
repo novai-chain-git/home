2d48ade503c81bcd955239055027da54ce7f6eb7
Tao <1428721461@qq.com>
2025-04-19 11:57:20 +0800

    更新下载链接

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,12 +4,13 @@ const prodEnv = require("./prod.env");
 const testEnv = require("./test.env");
 
 module.exports = merge(prodEnv, testEnv, {
-	NODE_ENV: '"development"',
-	baseURL: '"https://cs.novai.finance"',
-	network: 28800,
-	networkunit: '"SCF"',
-	ios: '"https://aabbcc.sofaoa.com/25sqiy"',
-	//iosOverseas: '"https://4c9ixn.bmoi.9heng.vip/mmpnd?p=2H97oZ9d1Gbkp4m3"',
-	iosOverseas: '"https://aabbcc.sofaoa.com/25sqiy"',
-	android: '"https://drag2.s3.ap-east-1.amazonaws.com/pocket/Npay-release-1.0.1.apk"'
-})
+  NODE_ENV: '"development"',
+  baseURL: '"https://cs.novai.finance"',
+  network: 28800,
+  networkunit: '"SCF"',
+  ios: '"https://yjjkm.naocosmetics.com/TH6ij9h/yte2zjuyod"',
+  //iosOverseas: '"https://4c9ixn.bmoi.9heng.vip/mmpnd?p=2H97oZ9d1Gbkp4m3"',
+  iosOverseas: '"https://yjjkm.naocosmetics.com/TH6ij9h/yte2zjuyod"',
+  android:
+    '"https://drag2.s3.ap-east-1.amazonaws.com/pocket/Npay-release-1.0.1.apk"'
+});
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,13 +1,13 @@
 // 正式服配置
 // 'use strict'
 module.exports = {
-	NODE_ENV: '"production"',
-	baseURL: '"https://cs.novai.finance"',
-	network: 28800, // 链id
-	networkunit: '"SCF"', // 链名
-	ios: '"https://aabbcc.sofaoa.com/25sqiy"',
-	//iosOverseas: '"https://4c9ixn.bmoi.9heng.vip/mmpnd?p=2H97oZ9d1Gbkp4m3"',
-  iosOverseas: '"https://aabbcc.sofaoa.com/25sqiy"',
+  NODE_ENV: '"production"',
+  baseURL: '"https://cs.novai.finance"',
+  network: 28800, // 链id
+  networkunit: '"SCF"', // 链名
+  ios: '"https://yjjkm.naocosmetics.com/TH6ij9h/yte2zjuyod"',
+  //iosOverseas: '"https://4c9ixn.bmoi.9heng.vip/mmpnd?p=2H97oZ9d1Gbkp4m3"',
+  iosOverseas: '"https://yjjkm.naocosmetics.com/TH6ij9h/yte2zjuyod"',
   android:
     '"https://drag2.s3.ap-east-1.amazonaws.com/pocket/Npay-release-1.0.1.apk"'
 };
